f34df0f5d8893a2ce0d03a439952ff23bdc9a178
Tamara Trotter <tltrotte@me.com>
2017-11-05 18:17:05 -0500

    start appends

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 $(document).ready(function() {
-console.log("Hi")
+  console.log("Hi")
 
-$.get('https://www.reddit.com/r/aww/.json', function(data){
-    console.log(data.data.children[0].data.title)});
+  $.get('https://www.reddit.com/r/aww/.json', function(data){
+    var main = data.data.children;
+    // main.forEach(function)
+  // first 10  post after initial post
+  for(i = 1; i<=10; i++){
+  var reddit = main[i];
+  var infoEl = $(".post");
 
-});
+  var title = $("<h3 id ='title'>");
+
+  var blog = $("<div id = 'blog'>");
+  infoEl.append(blog);
+
+  var content = $("<p> id = 'text' ");
+  blog.append(content);
+
+  var photo = $("<div id = 'photo'>");
+  blog.append(photo);
+
+
+
+
+
+}
+
+    //image source grab
+    // main.forEach(function(kid){
+    //   kid.data.preview.images.forEach(function(image){
+    //     console.log(image.source);
+      });
+
+    });
+
+  });
+
+}
